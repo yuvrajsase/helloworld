b083356bc1b535b2793a2d01d180eec8cc35012f
yuvrajsase <yuvrajsase@gmail.com>
2016-06-09 22:58:33 +0530

    Add files via upload

--- config/autoload/local.php
+++ config/autoload/local.php
@@ -1,14 +1,14 @@
 <?php
 /**
- * Global Configuration Override
+ * Local Configuration Override
  *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
+ * This configuration override file is for overriding environment-specific and
+ * security-sensitive configuration information. Copy this file without the
+ * .dist extension at the end and populate values as needed.
  *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
+ * @NOTE: This file is ignored from Git by default with the .gitignore included
+ * in ZendSkeletonApplication. This is a good practice, as it prevents sensitive
+ * credentials from accidentally being committed into version control.
  */
 
 return array(
